ea0069ffe84907d7407f8ed5e936b509c3006535
saidil <saidil@jakartamrt.co.id>
2025-04-24 13:27:03 +0700

    global state

---
+++ src/components/MultipleCounter.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Counter from "./Counter.vue";
+</script>
+
+<template>
+  <Counter />
+  <Counter />
+  <Counter />
+</template>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import "./style.css";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 import { createRouter, createWebHistory } from "vue-router";
-import Counter from "./components/Counter.vue";
+import MultipleCounter from "./components/MultipleCounter.vue";
 
 const pinia = createPinia();
 
 const router = createRouter({
-  routes: [{ path: "/counter", component: Counter }],
+  routes: [{ path: "/counter", component: MultipleCounter }],
   history: createWebHistory(),
 });
 
